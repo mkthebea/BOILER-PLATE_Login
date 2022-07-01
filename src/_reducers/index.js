b941304051d611fe2bc7976a5a4572864cdcd07f
mkthebea <5596molly@naver.com>
2022-07-02 00:20:28 +0900

    Setting Redux

---
+++ src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import user from './user_reducer'
+
+// 여러 리듀서를 rootReducer에서 하나로 합쳐주기
+const rootReducer = combineReducers({
+  //user,
+});
+
+export default rootReducer;
