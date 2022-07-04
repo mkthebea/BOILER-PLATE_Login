6df07ddf7b33bbd6842ea787fafb9b57209bab62
mkthebea <5596molly@naver.com>
2022-07-05 02:09:17 +0900

    authentication with hoc

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LanDingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
+          {/* Auth 사용 */}
+          {/* <Route path="/" element={Auth(LanDingPage, null)} /> */}
           <Route path="/" element={<LanDingPage />} />
+          {/* <Route path="/login" element={Auth(LoginPage,false)} /> */}
           <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/register" element={Auth(RegisterPage, false)} /> */}
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
